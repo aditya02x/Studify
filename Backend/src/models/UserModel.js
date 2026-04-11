a5251bc4ee1ee781b24e6bfc8cdf126212850cd2
Aditya Raj <adityaraj34@icloud.com>
2026-04-11 21:38:09 +0530

    user schema

---
+++ Backend/src/models/UserModel.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true  
+    },
+    email: {
+        type: String,   
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: { 
+        type:String,
+        enum:["student" , "teacher"],
+        required: true
+    },
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }],
+        createdAt: {
+        type: Date,
+        default: Date.now
+    ,}
+
+
+
+
+})
+
+export default mongoose.model('User',userSchema)
