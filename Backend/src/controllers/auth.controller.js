f7953af0d4a474ebea84122daf9456fa8cbc58ae
Aditya Raj <adityaraj34@icloud.com>
2026-04-12 00:27:00 +0530

    done with Loginr

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,4 +1,9 @@
+import { hash } from "bcrypt";
 import User from "../models/UserModel.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const registerUser = async (req,res)=>{
     try {
@@ -14,10 +19,12 @@ export const registerUser = async (req,res)=>{
         return res.status(400).json({message:"User Already Exist"})
     }
 
+    const hashedpassword = await bcrypt.hash(password,10)
+
     const newUser = new User({
         name,
         email,
-        password,
+        password: hashedpassword,
         role:"student"
     })
 
@@ -28,4 +35,40 @@ export const registerUser = async (req,res)=>{
         res.status(500).json({message:"Internal Server Error"})
     }
 
+}
+
+
+export const ooginUser = async (req,res)=>{
+    try {
+        const {email ,password} = req.body
+
+        if(!email || !password){
+            return res.status(400).json({message:"All Credintials Are required"})
+        }
+
+        const existUser = await User.findOne({email})
+        if(!existUser){
+            return res.status(400).json({message:"User Not Found"})
+        }
+
+        const isMatch = await bcrypt.compare(password, existUser.password)
+        if(!isMatch){
+            return res.status(400).json({message:"Invalid Credentials"})
+        }
+
+        const token = jwt.sign({id:existUser._id},process.env.JWT_SECRET,{expiresIn:"7d"})
+
+     
+
+        res.status(200).json({message:"Login Successful",token,user:{
+            id:existUser._id,
+            name:existUser.name,
+            email:existUser.email,
+            role:existUser.role
+        }})
+
+    } catch (error) {
+        console.log("Error in loginUser",error)
+        res.status(500).json({message:"Internal Server Error"})
+    }
 }
