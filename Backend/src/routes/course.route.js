c09e580da501576bed0f58cfab5ef78f03c645b1
Aditya Raj <adityaraj34@icloud.com>
2026-04-12 01:38:23 +0530

    fixed all eroor

--- Backend/src/routes/course.route.js
+++ Backend/src/routes/course.route.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { createCourse , getAllCourses , deleteCourse } from "../controllers/course.controller";
+import { createCourse , getAllCourses , deleteCourse , getSingleCourse } from "../controllers/course.controller.js";
+import authMiddleware from "../middleware/auth.middleware.js";
+import isInstructor from "../middleware/Instructor.middleware.js";
 const router = express.Router();
 
-router.post('/create',createCourse);
-router.get('/all',getAllCourses);
-router.delete('/delete/:id',deleteCourse)
+router.post('/', authMiddleware, isInstructor, createCourse);
+router.get('/', getAllCourses);
+router.get('/:id', getSingleCourse);
+router.delete('/:id', authMiddleware, isInstructor, deleteCourse);
 
 export default router;
