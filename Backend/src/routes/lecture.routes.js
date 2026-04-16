887d3ca94294d994de7a772e986775c67c38c4c4
Aditya Raj <adityaraj34@icloud.com>
2026-04-16 12:51:07 +0530

    deployment

---
+++ Backend/src/routes/lecture.routes.js
@@ -0,0 +1,9 @@
+import authMiddleware from "../middleware/auth.middleware.js";
+import express from "express";
+import { createLecture , getLecturesByCourse , deleteLecture } from "../controllers/Lecture.controller.js";
+import isInstructor from "../middleware/Instructor.middleware.js";
+const router = express.Router();
+router.post('/create', authMiddleware, isInstructor, createLecture);
+router.get('/:courseId', authMiddleware, getLecturesByCourse);
+router.delete('/:lectureId', authMiddleware, isInstructor, deleteLecture);
+export default router;
