80c574e82ea2ac9265b6721e612ddc8c110f16b4
Aditya Raj <adityaraj34@icloud.com>
2026-04-16 23:04:55 +0530

    error solved

--- Frontend/src/pages/auth/Login.jsx
+++ Frontend/src/pages/auth/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { toast } from "react-toastify";
 import { useNavigate, Link } from "react-router-dom";
-import api from "../../services/api.js";
+import api from "../../Services/api.js";
 
 const Login = () => {
   const [email, setEmail] = React.useState("");
