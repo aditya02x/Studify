74d43d23b52caebb47fcfd0da3e851f7a00d32b7
Aditya Raj <adityaraj34@icloud.com>
2026-04-17 23:27:36 +0530

    added route fro comment

--- Backend/src/routes/lecture.routes.js
+++ Backend/src/routes/lecture.routes.js
@@ -1,9 +1,10 @@
 import authMiddleware from "../middleware/auth.middleware.js";
 import express from "express";
-import { createLecture , getLecturesByCourse , deleteLecture } from "../controllers/Lecture.controller.js";
+import { createLecture , getLecturesByCourse , deleteLecture , CreateComment } from "../controllers/Lecture.controller.js";
 import isInstructor from "../middleware/Instructor.middleware.js";
 const router = express.Router();
 router.post('/create', authMiddleware, isInstructor, createLecture);
 router.get('/:courseId', authMiddleware, getLecturesByCourse);
 router.delete('/:lectureId', authMiddleware, isInstructor, deleteLecture);
+router.post('/:lectureId/comments', authMiddleware, CreateComment);
 export default router;
