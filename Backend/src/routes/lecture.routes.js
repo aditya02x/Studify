dfc8aff463dadc2a7016a8f75612eaa02ca96d28
Aditya Raj <adityaraj34@icloud.com>
2026-04-17 23:53:04 +0530

    comment schema sdgsgddgdgfda

--- Backend/src/routes/lecture.routes.js
+++ Backend/src/routes/lecture.routes.js
@@ -13,3 +13,10 @@ const router = express.Router();
 
 
 router.post('/:lectureId/comments', authMiddleware, CreateComment);
+router.get('/:lectureId/comments', authMiddleware, getCommentsByLecture);
+
+router.post('/create', authMiddleware, isInstructor, createLecture);
+router.delete('/:lectureId', authMiddleware, isInstructor, deleteLecture);
+router.get('/:courseId', authMiddleware, getLecturesByCourse);
+
+export default router;
