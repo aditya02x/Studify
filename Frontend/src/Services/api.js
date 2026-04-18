3ff9ce440aca51d2d27c53c44a282a39c9d5e7aa
Aditya Raj <adityaraj34@icloud.com>
2026-04-18 13:47:48 +0530

    axiso error fixed

--- Frontend/src/Services/api.js
+++ Frontend/src/Services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://studify-39te.onrender.com/api",,
+  baseURL: "https://studify-39te.onrender.com/api",
+  withCredentials: true
 });
 
 // ✅ ADD TOKEN AUTOMATICALLY
