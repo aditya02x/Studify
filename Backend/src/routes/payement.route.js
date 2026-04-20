17a5c15b01239f313d4b4316739702f1239f2bde
Aditya Raj <adityaraj34@icloud.com>
2026-04-20 23:51:31 +0530

    router payment

---
+++ Backend/src/routes/payement.route.js
@@ -0,0 +1,9 @@
+import express, { Router } from 'express';
+import { createOrder, verifyPayment } from '../controllers/payement.controller.js';
+import { authMiddleware } from '../middlewares/auth.middleware.js';
+
+const router = express.Router()
+router.post('/create-order',authMiddleware,createOrder);
+router.post("verify",authMiddleware,verifyPayment)
+
+export router
