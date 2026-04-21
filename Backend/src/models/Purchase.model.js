62e55c3c462fe15c6cbe5f84ef01ab00e8d58114
Aditya Raj <adityaraj34@icloud.com>
2026-04-21 23:52:08 +0530

    update model

--- Backend/src/models/Purchase.model.js
+++ Backend/src/models/Purchase.model.js
@@ -1,14 +1,20 @@
 import mongoose from "mongoose";
 
 const purchaseSchema = new mongoose.Schema({
-    userId:{
-        type:String,
-        required:true
-    },
-    courseId:{
-        type:String,
-        required:true
-    }
-},{ timestamps: true });
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  courseId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Course",
+    required: true,
+  },
+  paymentId: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
 
-export default mongoose.model('Purchase', purchaseSchema);
+export default mongoose.model("Purchase", purchaseSchema);
