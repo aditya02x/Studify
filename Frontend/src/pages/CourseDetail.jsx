14849b693049e036dfa8e0bfa75697e21ffc682a
Aditya Raj <adityaraj34@icloud.com>
2026-04-24 23:37:06 +0530

    dggdfdf

--- Frontend/src/pages/CourseDetail.jsx
+++ Frontend/src/pages/CourseDetail.jsx
@@ -34,7 +34,17 @@ const CourseDetail = () => {
   const handleBuy = async () => {
     try {
       const { data: order } = await api.post("/payment/create-order", {
-        courseId: id
+        courseId: id,
+      });
+
+      const options = {
+        key: import.meta.env.VITE_RAZORPAY_KEY_ID,
+        amount: order.amount,
+        currency: "INR",
+        name: "Studify",
+        description: course.title,
+
+handler: async function (response) {
   console.log("FULL RESPONSE:", response); // debug
 
     razorpay_payment_id: response.razorpay_payment_id,
