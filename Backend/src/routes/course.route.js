2912c4596759ddeed3cc662cf9a551c3dd2aee1e
Aditya Raj <adityaraj34@icloud.com>
2026-04-26 16:43:07 +0530

    fixed the course

--- Backend/src/routes/course.route.js
+++ Backend/src/routes/course.route.js
@@ -1,13 +1,14 @@
 import express from "express";
-import { createCourse , getAllCourses , deleteCourse , getSingleCourse,getMyCourses } from "../controllers/course.controller.js";
+import { createCourse, getAllCourses, deleteCourse, getSingleCourse, getMyCourses } from "../controllers/course.controller.js";
 import authMiddleware from "../middleware/auth.middleware.js";
 import isInstructor from "../middleware/Instructor.middleware.js";
+
 const router = express.Router();
 
 router.post('/', authMiddleware, isInstructor, createCourse);
 router.get('/', getAllCourses);
-router.get('/:id', getSingleCourse);
+router.get('/my-courses', authMiddleware, getMyCourses); // ✅ MOVED UP - before /:id
+router.get('/:id', getSingleCourse);                     // ✅ now below
 router.delete('/:id', authMiddleware, isInstructor, deleteCourse);
-router.get('/my-courses', authMiddleware, getMyCourses);
 
 export default router;
