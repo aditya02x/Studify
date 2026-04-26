16bb4ec403947b37ca67e8e00161e5ffe987cec3
Aditya Raj <adityaraj34@icloud.com>
2026-04-26 12:36:19 +0530

    dgf

--- Frontend/src/Services/api.js
+++ Frontend/src/Services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "https://studify-1-ww39.onrender.com",
-  baseURL: "https://studify-1-ww39.onrender.com/api"
+  baseURL: "https://studify-1-ww39.onrender.com/api",
   withCredentials: true
 });
 
